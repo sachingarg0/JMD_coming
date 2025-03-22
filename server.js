50fd573447a7b462e0db1dc01c108bc05a01085c
sachingarg0 <aggarwal7300@gmail.com>
2025-03-22 14:53:54 +0530

    jmd

---
+++ server.js
@@ -0,0 +1,20 @@
+// Import required modules
+const express = require('express');
+const path = require('path');
+
+// Initialize the app
+const app = express();
+const PORT = 3000;
+
+// Serve static HTML file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
